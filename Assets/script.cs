b611b2302e2852f67f5bc7f72a23328effba8408
andreziin00 <97922478+Andreziin00@users.noreply.github.com>
2022-08-17 19:58:04 -0300

    commit comum

---
+++ Assets/script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour;
+
+    public float jumpForce;
+    private bool doubleJump, isJump, isGround;
+    private Transform groudcheck;
+    private RigidBody2D rb;
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<RigidBody2D> ();
+        groundcheck = GameObject.Find ("groundcheck").transform;
+    }
+
+    void
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
