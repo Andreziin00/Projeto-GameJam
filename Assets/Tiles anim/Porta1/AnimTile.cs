a96675ea45289c3e31de164fbb4c2186c170324f
ArknoXI <contato.arkno@gmail.com>
2022-08-28 10:38:39 -0300

    Patio feito e corredor de cima criado
    N é a versão definitiva, posso passo é criar os cenarios da sala.

---
+++ Assets/Tiles anim/Porta1/AnimTile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Tile.../Animated Tile", fileName = "New anim tile")]
+public class AnimTile: TileBase
+{
+ public Sprite[] sprites;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        if(sprites != null && sprites.Length > 0)
+        {
+         tileData.sprite = sprites[0];
+        }
+    }
+
+    public override bool GetTileAnimationData(Vector3Int position, ITilemap tilemap, ref TileAnimationData tileAnimationData)
+    {
+        if(sprites.Length > 0)
+        {
+         tileAnimationData.animatedSprites = sprites;
+         tileAnimationData.animationSpeed = 1;
+         tileAnimationData.animationStartTime = 0;
+         return true;
+        }
+        return false;
+    }
+}
--- Assets/script.cs
+++ Assets/script.cs
@@ -2,22 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jump : MonoBehaviour;
-
-    public float jumpForce;
+public class jump : MonoBehaviour
+{
+    //public float jumpForce;
     private bool doubleJump, isJump, isGround;
     private Transform groudcheck;
-    private RigidBody2D rb;
-
-{
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    //Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<RigidBody2D> ();
-        groundcheck = GameObject.Find ("groundcheck").transform;
+        rb = GetComponent<Rigidbody2D>();
+        groudcheck = GameObject.Find ("groundcheck").transform;
     }
-
-    void
     // Update is called once per frame
     void Update()
     {
