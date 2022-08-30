4de7ce132a2b3c948732ab870484c6872324a7ab
andreziin00 <97922478+Andreziin00@users.noreply.github.com>
2022-08-30 09:55:04 -0300

    personagem
    adicionada movimentação do personagem

---
+++ Assets/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int speed = 5;
+    Vector2 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+    }
+}
