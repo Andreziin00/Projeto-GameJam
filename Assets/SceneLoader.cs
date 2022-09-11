893c0661744ddc8199e9b2f727731d9451699ed8
ArknoXI <contato.arkno@gmail.com>
2022-09-11 14:48:22 -0300

    player + transição

--- Assets/SceneLoader.cs
+++ Assets/SceneLoader.cs
@@ -3,24 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Escadaria : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
        if(collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(0);
         }
     }
 }
